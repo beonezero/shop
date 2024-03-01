3648a6d81399dd90cca5f15dbc5a84e8963358f6
Neakhai Yauheni <beonezero@protonmail.com>
2024-03-01 16:48:11 +0300

    create Page404.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
-import {Route, Routes} from "react-router-dom";
-import {Main} from "./Components/Main";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {HomePage} from "./Components/HomePage";
 import {Login} from "./Components/Login";
+import {Page404} from "./Components/Page404";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-          <Route path={"/"} element={<Main/>}/>
-          <Route path={"/login"} element={<Login/>}/>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path={"/"} element={<HomePage/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/404"} element={<Page404/>}/>
+                <Route path={"*"} element={<Navigate to={"/404"}/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/HomePage.tsx
@@ -0,0 +1,8 @@
+import {Navigate} from "react-router-dom";
+import {useState} from "react";
+
+export const HomePage = () => {
+    return <div>
+        HomePage
+    </div>
+}
